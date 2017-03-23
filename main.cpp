a632125f7e746ad73a3c517965c37d07567e0764
liuqun <517067180@qq.com>
2017-03-23 09:24:52 +0800

    创建测试框架程序

--- main.cpp
+++ main.cpp
@@ -1,34 +1,162 @@
+/* encoding: utf-8 */  // 使用 UTF-8 汉字编码
+#ifndef __cplusplus // 调用 C stdio.h 等标准库
+# include <stdio.h>
+# include <stdlib.h>
+# include <ctype.h>
+#else
+# include <cstdio>
+# include <cstdlib>
+# include <cctype>
+using namespace std;
+#endif
+
 #include <sapi/tpm20.h>
 
 #include "debug.h"
 #include "tcti_util.h"
 
+/* 自定义函数 */
+static void DoMyTestsWithTctiContext(TSS2_TCTI_CONTEXT *pTctiContext);
 
-int main(/* int argc, char **argv */)
+/* 以下函数均使用4个空格缩进，不使用Tab缩进 */
+
+static void PrintHelp()
+{
+    const char *version = "0.01";
+
+    printf("My TPM client test app, Version %s\n", version);
+    printf("用法:\n");
+    printf("tpmclient [-rmhost hostname|IP address] [-rmport port]\n\n");
+    printf("其中:\n");
+    printf("-rmhost 手动指定运行资源管理器(即 resourcemgr)的主机IP地址或主机名 (默认值: %s)\n",
+            DEFAULT_HOSTNAME);
+    printf("-rmport 手动指定运行资源管理器的主机端口号 (默认值: %d)\n", DEFAULT_RESMGR_TPM_PORT);
+}
+
+int main(int argc, char *argv[])
 {
     TSS2_RC rval;
-    TCTI_SOCKET_CONF rmInterfaceConfig = {
-        DEFAULT_HOSTNAME,
-        DEFAULT_RESMGR_TPM_PORT,
-        DebugPrintfCallback,
-        DebugPrintBufferCallback,
-        NULL
-    };
+    TCTI_SOCKET_CONF rmInterfaceConfig;
     TSS2_TCTI_CONTEXT *pTctiContext;
-    TSS2_TCTI_CONTEXT **ppTctiContext = &pTctiContext;
+    int count;
 
-    rval = InitSocketTctiContext(&rmInterfaceConfig, ppTctiContext);
+    rmInterfaceConfig.hostname = DEFAULT_HOSTNAME;
+    rmInterfaceConfig.port = DEFAULT_RESMGR_TPM_PORT;
+    rmInterfaceConfig.logCallback = DebugPrintfCallback;
+    rmInterfaceConfig.logBufferCallback = DebugPrintBufferCallback;
+    rmInterfaceConfig.logData = NULL;
+
+    count = 1;
+    while (count < argc)
+    {
+        if (0 == strcmp(argv[count], "-rmhost"))
+        {
+            if (count + 1 >= argc)
+            {
+                PrintHelp();
+                return 1;
+            }
+            rmInterfaceConfig.hostname = argv[count + 1];  // 暂时不检查无效的输入参数
+            count += 2;
+        }
+        else if (0 == strcmp(argv[count], "-rmport"))
+        {
+            if (count + 1 >= argc)
+            {
+                PrintHelp();
+                return 1;
+            }
+            rmInterfaceConfig.port = strtoul(argv[count + 1], NULL, 10); // 暂时不检查无效的输入参数
+            count += 2;
+        }
+        else
+        {
+            PrintHelp();
+            return -1;
+        }
+    }
+    // 以上代码提供了一组简单的命令行参数便于调试:
+    // 其中包括 [-rmhost IP地址] 和 [-rmport 端口号]
+    // 如果不指定命令行参数, 则会直接连接到本机 IP 地址默认端口上运行的资源管理器
+
+    /**/
+    rval = InitSocketTctiContext(&rmInterfaceConfig, &pTctiContext);
     if (rval != TSS2_RC_SUCCESS)
     {
-        DebugPrintf(NO_PREFIX, "initialization failed: rval=0x%x.  Exiting...\n", rval);
-	return (-1);
+        // Note:
+        // 当前 InitSocketTcti() 返回的 TSS2_RC 值并未严格按照 TPM2.0 规范指示错误原因。
+        // 错误返回值实测结果: 无法连接服务器端 IP 地址或默认端口号无法建立套接字时, 返回值均等于 1
+        DebugPrintf(NO_PREFIX,
+                "TCTI context initialization failed with error return code=0x%x\n",
+                rval);
+        return (-1);
+    }
+    else if (!pTctiContext)
+    {
+        DebugPrintf(NO_PREFIX, "TCTI context initialization failed\n");
+        return (-1);
+    }
+
+    /* 使用前面创建的 TCTI 上下文对象进一步创建其他测试内容 */
+    DoMyTestsWithTctiContext(pTctiContext);
+
+    /* 测试结束后销毁 TCTI 上下文对象 */
+    TeardownTctiContext(&pTctiContext);
+    return (0);
+}
+
+inline TSS2_SYS_CONTEXT *pSysContextFromPVoid(void *pVoid)
+{
+#ifdef __cplusplus
+    return static_cast<TSS2_SYS_CONTEXT*>(pVoid);
+#else
+    return (pVoid);
+#endif
+}
+
+static void DoMyTestsWithTctiContext(TSS2_TCTI_CONTEXT *pTctiContext)
+{
+    TSS2_RC rval;
+    TSS2_ABI_VERSION abiVersion;
+    TSS2_SYS_CONTEXT *pSysContext;
+    size_t contextSize;
+
+    contextSize = Tss2_Sys_GetContextSize(0);
+    pSysContext = pSysContextFromPVoid(malloc(contextSize));
+    if (!pSysContext)
+    {
+        DebugPrintf(NO_PREFIX,
+                "Unable to allocate enough memory: malloc() failed.\n");
+        DebugPrintf(NO_PREFIX, "Exiting...\n");
+        return;
+    }
+
+    abiVersion.tssCreator = TSSWG_INTEROP;
+    abiVersion.tssFamily = TSS_SAPI_FIRST_FAMILY;
+    abiVersion.tssLevel = TSS_SAPI_FIRST_LEVEL;
+    abiVersion.tssVersion = TSS_SAPI_FIRST_VERSION;
+
+    rval = Tss2_Sys_Initialize(pSysContext, contextSize, pTctiContext,
+            &abiVersion);
+
+    if (rval != TSS2_RC_SUCCESS)
+    {
+        free(pSysContext);
+        DebugPrintf(NO_PREFIX,
+                "Unable to initialize system level API context:");
+        DebugPrintf(NO_PREFIX,
+                "Tss2_Sys_Initialize() returns error code 0x%06X.\n", rval);
+        DebugPrintf(NO_PREFIX, "Exiting...\n");
+        return;
     }
 
     /*
      * TODO: Add more tests here
      */
 
-    return (0);
+    /* Clean up the context when all tests ends */
+    Tss2_Sys_Finalize(pSysContext);
+    free(pSysContext);
+    pSysContext = NULL;
+    return;
 }
-
-
