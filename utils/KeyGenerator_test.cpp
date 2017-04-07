794feaf1174c0fe7bc47a7177ae193892f9e09f0
liuqun <qunliu@zyhx-group.com>
2017-04-07 18:32:18 +0800

    创建 class KeyPublicDataReadingOperation
    测试对 ReadPublic 函数相关的数据结构体进行封装

--- utils/KeyGenerator_test.cpp
+++ utils/KeyGenerator_test.cpp
@@ -156,6 +156,71 @@ static void DoMyTestsWithTctiContext(TSS2_TCTI_CONTEXT *pTctiContext)
     return;
 }
 
+/**
+ * 访问密钥节点公开信息
+ */
+class KeyPublicDataReadingOperation {
+private:
+    TPMI_DH_OBJECT keyHandle;
+    // 上面的成员变量用于保存输入参数
+    // 下面的成员变量用于保存输出结果
+    TPM2B_PUBLIC keyPublicData; // 内部包含 TPM 定义的巨型数据结构
+    TPM2B_NAME keyName;
+    TPM2B_NAME qualifiedName;
+    TPM_RC rc;
+public:
+    KeyPublicDataReadingOperation() {
+        keyHandle = (TPM_HANDLE) 0;
+        // 输出结果初始化
+        keyPublicData.t.size = 0;
+        keyName.t.name[0] = '\0'; // Used for debugging
+        qualifiedName.t.name[0] = '\0'; // Used for debugging
+        rc = TPM_RC_SUCCESS;
+    }
+    ~KeyPublicDataReadingOperation() {
+    }
+
+    /**
+     * 通过句柄指定访问的密钥节点, 同时保存相应的访问授权数据
+     *
+     * @param handle 指定句柄, 取值一般为 0x81 或 0x80 开头
+     * @return TPMI_DH_OBJECT 仅用于调试, 返回值总是等于参数列表中指定的句柄
+     */
+    TPMI_DH_OBJECT setKeyHandle(TPMI_DH_OBJECT handle) {
+        this->keyHandle = handle;
+        return handle;
+    }
+
+    /**
+     * 执行 TPM 命令
+     */
+    void execute(TSS2_SYS_CONTEXT *pSysContext) {
+        /* 调用 TPM 命令 */
+        keyName.t.size = sizeof(keyName) - sizeof(qualifiedName.t.size);
+        qualifiedName.t.size = sizeof(qualifiedName) - sizeof(qualifiedName.t.size);
+        rc =  Tss2_Sys_ReadPublic(
+                pSysContext,
+                keyHandle, // IN
+                (TSS2_SYS_CMD_AUTHS *) NULL, // 读取公开数据不需要授权, 另外只有输出参数没有输入参数
+                &keyPublicData, // OUT
+                &keyName, // OUT
+                &qualifiedName, // OUT
+                (TSS2_SYS_RSP_AUTHS *) NULL // 回传输出参数时可选择是否加密传输, 但这里可以暂时不实现该选项
+                );
+        if (rc) {
+            // fprintf(stderr, "Error: rc=0x%X\n", rc);
+            throw (TSS2_RC) rc;
+        }
+        return;
+    }
+    /**
+     *
+     */
+    const TPM2B_NAME& getKeyName() {
+        return keyName;
+    }
+};
+
 static void DoMyTestsWithSysContext(TSS2_SYS_CONTEXT *pSysContext)
 {
     TSS2_SYS_CONTEXT *sysContext = pSysContext;
