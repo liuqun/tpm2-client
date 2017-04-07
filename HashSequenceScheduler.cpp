a2cc20cc7a47c359a94108026bf5d95c8adf1442
liuqun <qunliu@zyhx-group.com>
2017-04-07 10:45:31 +0800

    提取 HashSequenceCompleteCommand 类的定义到头文件中去

--- HashSequenceScheduler.cpp
+++ HashSequenceScheduler.cpp
@@ -80,196 +80,6 @@ void HashSequenceScheduler::update(const TPM2B_MAX_BUFFER *pMessagePacket)
     }
 }
 
-/**
- * C++ 类名: HashSequenceCompleteCommand
- *
- * 将 Tss2_Sys_SequenceComplete 的所有函数参数封装在一起,
- * 避免调用该函数时需额外创建一堆零散的数据结构作为参数
- */
-class HashSequenceCompleteCommand {
-private:
-    TPMI_DH_OBJECT sequenceHandle;
-    TPMS_AUTH_COMMAND sessionData;
-    TPM2B_MAX_BUFFER data; // 用于存储最后一轮哈希操作中待处理的原始数据 (可以为空)
-    TPMI_RH_HIERARCHY hierarchy; // The hierarchy of the ticket for a hash operation
-    // 以上成员变量用于存储输入数据
-    // 以下用于记录输出结果
-    TPM2B_DIGEST result; // 哈希摘要运算结果
-    TPMT_TK_HASHCHECK validationTicket; // 附带一份哈希结果有效性证明, 用于后续调用相关命令对摘要结果进行签名
-    TPM_RC rc;
-
-public:
-    HashSequenceCompleteCommand() {
-        sequenceHandle = (TPM_HT_NONE << HR_SHIFT);
-        sessionData.sessionHandle = TPM_RS_PW;
-        sessionData.nonce.t.size = 0;
-        sessionData.sessionAttributes.val = 0;
-        sessionData.hmac.t.size = 0;
-        sessionData.hmac.t.buffer[0] = '\0'; // Used for debugging
-        data.t.size = 0;
-        data.t.buffer[0] = '\0'; // Used for debugging
-        hierarchy = TPM_RH_PLATFORM;
-        result.t.size = 0; // Used for debugging
-        result.t.buffer[0] = '\0'; // Used for debugging
-        rc = TPM_RC_SUCCESS;
-    }
-    virtual ~HashSequenceCompleteCommand() {
-        clearAuthValue();
-        clearFinalDataPack();
-    }
-
-public:
-    void setSequenceHandle(
-            TPMI_DH_OBJECT sequenceHandle // 句柄
-            ) {
-        // FIXME 检查句柄 sequenceHandle 的有效值
-        const TPM_HT type = (sequenceHandle & HR_RANGE_MASK) >> HR_SHIFT;
-        if (type != TPM_HT_TRANSIENT) {
-            /* 当遇到无效的句柄时, 子函数将抛出异常 0x0103 TPM_RC_SEQUENCE */
-            throw (TSS2_RC) TPM_RC_SEQUENCE;
-        }
-        this->sequenceHandle = sequenceHandle;
-    }
-    void setSequenceHandleWithOptionalAuthValue(
-            TPMI_DH_OBJECT sequenceHandle, // 句柄
-            BYTE authValue[], // 访问该句柄时所需的授权数据
-            UINT16 size       // 授权数据长度
-            ) {
-        // FIXME 检查句柄 sequenceHandle 的有效值
-        const TPM_HT type = (sequenceHandle & HR_RANGE_MASK) >> HR_SHIFT;
-        if (type != TPM_HT_TRANSIENT) {
-            /* 当遇到无效的句柄时, 子函数将抛出异常 0x0103 TPM_RC_SEQUENCE */
-            throw (TSS2_RC) TPM_RC_SEQUENCE;
-        }
-        this->sequenceHandle = sequenceHandle;
-
-        if (size > sizeof(sessionData.hmac.t.buffer)) {
-            /* 自动截断并舍弃超过长度上限的数据 */
-            size = sizeof(sessionData.hmac.t.buffer);
-        }
-        sessionData.hmac.t.size = size;
-        memcpy(sessionData.hmac.t.buffer, authValue, size);
-    }
-    void clearAuthValue() {
-        const size_t len = sizeof(sessionData.hmac);
-        memset(&(sessionData.hmac), 0x00, len); // 清空残留数据
-    }
-
-    /**
-     * 存入待进行哈希计算的最后一个数据包
-     *
-     * @param data 待哈希的数据包
-     * @param size 如果数据长度超过 MAX_DIGEST_BUFFER=1024 字节, 这里将自动截断多余的字节
-     * @return TPM2B 格式的数据块, 仅为调试使用, 可直接忽略该返回值
-     */
-    const TPM2B_MAX_BUFFER& prepareFinalDataPack(const BYTE data[], UINT16 size) {
-        if (size > MAX_DIGEST_BUFFER) {
-            /* 自动截断并舍弃超过长度上限的数据 */
-            size = MAX_DIGEST_BUFFER;
-        }
-        this->data.t.size = size;
-        memcpy(this->data.t.buffer, data, size);
-        return this->data;
-    }
-    /**
-     * 清除数据
-     */
-    void clearFinalDataPack() {
-        const size_t len = sizeof(data);
-        memset(&data, 0x00, len); // 清空残留数据
-    }
-
-    /**
-     * 执行 TPM 命令
-     */
-    virtual void execute(TSS2_SYS_CONTEXT *pSysContext) {
-        if (data.t.size > MAX_DIGEST_BUFFER) {
-            /* 检查待处理的字节数 */
-            throw (TSS2_RC) TSS2_SYS_RC_BAD_VALUE;
-        }
-        if (HR_NONE == (TPM_HC) sequenceHandle) {
-            /* 检查句柄 sequenceHandle 的有效值 */
-            throw (TSS2_RC) TPM_RC_SEQUENCE;
-        }
-
-        TPMS_AUTH_COMMAND *cmdAuths[1];
-        TSS2_SYS_CMD_AUTHS cmdAuthsArray;
-        cmdAuths[0] = &sessionData;
-        cmdAuthsArray.cmdAuths = cmdAuths;
-        cmdAuthsArray.cmdAuthsCount = 1;
-
-        TPMS_AUTH_RESPONSE sessionDataOut;
-        TPMS_AUTH_RESPONSE *rspAuths[1];
-        TSS2_SYS_RSP_AUTHS rspAuthsArray;
-        rspAuths[0] = &sessionDataOut;
-        rspAuthsArray.rspAuths = rspAuths;
-        rspAuthsArray.rspAuthsCount = 1;
-
-        /* 调用 TPM 命令 */
-        result.t.size = sizeof(result.t.buffer);
-        rc = Tss2_Sys_SequenceComplete(
-                pSysContext, //
-                sequenceHandle, // IN
-                &cmdAuthsArray, // IN
-                &data, // IN
-                hierarchy, // IN
-                &result, // OUT
-                &validationTicket, // OUT
-                &rspAuthsArray); //
-        if (rc) {
-            throw (TSS2_RC) rc;
-            // fprintf(stderr, "Error: rc=0x%X\n", rc);
-        }
-        return;
-    }
-
-    /**
-     * 取出最终哈希摘要计算结果数据缓冲区指针
-     *
-     * @return 缓冲区首地址指针
-     */
-    const BYTE *getResponseDigestValue() const {
-        return result.t.buffer;
-    }
-
-    /**
-     * 取出最终哈希摘要计算结果数据缓冲区的长度, 单位字节
-     *
-     * @return 长度
-     */
-    UINT16 getResponseDigestSize() const {
-        return result.t.size;
-    }
-
-    /**
-     * 取出最终哈希摘要计算结果 TPM2B 结构体
-     *
-     * @return TPM2B_DIGEST (C++引用)
-     */
-    const TPM2B_DIGEST& getResponseDigest() const {
-        return result;
-    }
-
-    /**
-     * 指定哈希操作输出凭据中引用的 hierarchy 树
-     *
-     * @param hierarchy 可以选择 TPM_RH_PLATFORM 或 TPM_RH_OWNER 等
-     * @return TPMI_RH_HIERARCHY 仅用于方便调试
-     */
-    TPMI_RH_HIERARCHY setHierarchyForValidationTicket(TPMI_RH_HIERARCHY hierarchy) {
-        this->hierarchy = hierarchy;
-        return hierarchy;
-    }
-     /**
-     * 取出证明哈希摘要是由 TPM 模块输出的凭据
-     *
-     * @return TPMT_TK_HASHCHECK (C++引用) 凭据
-     */
-    const TPMT_TK_HASHCHECK& getResponseValidationTicket() const {
-        return validationTicket;
-    }
-};
-
 void HashSequenceScheduler::complete(TPM2B_DIGEST *result)
 {
     if (!m_started)
@@ -504,3 +314,185 @@ void HashSequenceUpdateCommand::execute(TSS2_SYS_CONTEXT *pSysContext) {
     }
     return;
 }
+
+// -------------------------------------------------------
+// 以下为 C++ class HashSequenceCompleteCommand 的实现代码
+/**
+ * 构造函数
+ */
+HashSequenceCompleteCommand::HashSequenceCompleteCommand() {
+    sequenceHandle = (TPM_HT_NONE << HR_SHIFT);
+    sessionData.sessionHandle = TPM_RS_PW;
+    sessionData.nonce.t.size = 0;
+    sessionData.sessionAttributes.val = 0;
+    sessionData.hmac.t.size = 0;
+    sessionData.hmac.t.buffer[0] = '\0'; // Used for debugging
+    data.t.size = 0;
+    data.t.buffer[0] = '\0'; // Used for debugging
+    hierarchy = TPM_RH_PLATFORM;
+    result.t.size = 0; // Used for debugging
+    result.t.buffer[0] = '\0'; // Used for debugging
+    rc = TPM_RC_SUCCESS;
+}
+
+/**
+ * 析构函数
+ */
+HashSequenceCompleteCommand::~HashSequenceCompleteCommand() {
+    clearAuthValue();
+    clearFinalDataPack();
+}
+
+/**
+ * 指定哈希序列句柄(但不设置句柄本身的访问授权值)
+ */
+void HashSequenceCompleteCommand::setSequenceHandle(
+        TPMI_DH_OBJECT sequenceHandle // 句柄
+        ) {
+    const TPM_HT type =
+            (sequenceHandle & HR_RANGE_MASK) >> HR_SHIFT;
+    if (type != TPM_HT_TRANSIENT) {
+        /* 当遇到无效的句柄时, 子函数将抛出异常 0x103 TPM_RC_SEQUENCE */
+        throw (TSS2_RC) TPM_RC_SEQUENCE;
+    }
+    this->sequenceHandle = sequenceHandle;
+}
+
+/**
+ * 指定哈希序列句柄, 同时指定句柄本身的访问授权 AuthValue
+ */
+void HashSequenceCompleteCommand::setSequenceHandleWithOptionalAuthValue(
+        TPMI_DH_OBJECT sequenceHandle, // 句柄
+        BYTE authValue[], // 句柄授权数据
+        UINT16 size // 数据长度
+        ) {
+    const TPM_HT type =
+            (sequenceHandle & HR_RANGE_MASK) >> HR_SHIFT;
+    if (type != TPM_HT_TRANSIENT) {
+        /* 当遇到无效的句柄时, 子函数将抛出异常 0x0103 TPM_RC_SEQUENCE */
+        throw (TSS2_RC) TPM_RC_SEQUENCE;
+    }
+    this->sequenceHandle = sequenceHandle;
+
+    if (size > sizeof(sessionData.hmac.t.buffer)) {
+        /* 自动截断并舍弃超过长度上限的数据 */
+        size = sizeof(sessionData.hmac.t.buffer);
+    }
+    sessionData.hmac.t.size = size;
+    memcpy(sessionData.hmac.t.buffer, authValue, size);
+}
+
+/**
+ * 清除之前指定的句柄访问授权 AuthValue, 以免泄露敏感数据
+ */
+void HashSequenceCompleteCommand::clearAuthValue() {
+    const size_t len = sizeof(sessionData.hmac);
+    memset(&(sessionData.hmac), 0x00, len); // 清空残留数据
+}
+
+/**
+ * 存入待进行哈希计算的最后一个数据包
+ *
+ * 具体参数及返回值以头文件中的定义为准
+ */
+const TPM2B_MAX_BUFFER& HashSequenceCompleteCommand::prepareFinalDataPack(const BYTE data[], UINT16 size){
+    if (size > MAX_DIGEST_BUFFER) {
+        /* 自动截断并舍弃超过长度上限的数据 */
+        size = MAX_DIGEST_BUFFER;
+    }
+    this->data.t.size = size;
+    memcpy(this->data.t.buffer, data, size);
+    return this->data;
+}
+
+/**
+ * 清除数据(清除运行时缓存的最后一个待计算的原始数据包)
+ */
+void HashSequenceCompleteCommand::clearFinalDataPack() {
+    const size_t len = sizeof(data);
+    memset(&data, 0x00, len); // 清空残留数据
+}
+
+/**
+ * 执行 TPM 命令
+ */
+void HashSequenceCompleteCommand::execute(TSS2_SYS_CONTEXT *pSysContext) {
+    if (data.t.size > MAX_DIGEST_BUFFER) {
+        /* 检查待处理的字节数 */
+        throw (TSS2_RC) TSS2_SYS_RC_BAD_VALUE;
+    }
+    if (HR_NONE == (TPM_HC) sequenceHandle) {
+        /* 检查句柄 sequenceHandle 的有效值 */
+        throw (TSS2_RC) TPM_RC_SEQUENCE;
+    }
+
+    TPMS_AUTH_COMMAND *cmdAuths[1];
+    TSS2_SYS_CMD_AUTHS cmdAuthsArray;
+    cmdAuths[0] = &sessionData;
+    cmdAuthsArray.cmdAuths = cmdAuths;
+    cmdAuthsArray.cmdAuthsCount = 1;
+
+    TPMS_AUTH_RESPONSE sessionDataOut;
+    TPMS_AUTH_RESPONSE *rspAuths[1];
+    TSS2_SYS_RSP_AUTHS rspAuthsArray;
+    rspAuths[0] = &sessionDataOut;
+    rspAuthsArray.rspAuths = rspAuths;
+    rspAuthsArray.rspAuthsCount = 1;
+
+    /* 调用 TPM 命令 */
+    result.t.size = sizeof(result.t.buffer);
+    rc = Tss2_Sys_SequenceComplete(
+            pSysContext, //
+            sequenceHandle, // IN
+            &cmdAuthsArray, // IN
+            &data, // IN
+            hierarchy, // IN
+            &result, // OUT
+            &validationTicket, // OUT
+            &rspAuthsArray); //
+    if (rc) {
+        throw (TSS2_RC) rc;
+        // fprintf(stderr, "Error: rc=0x%X\n", rc);
+    }
+    return;
+}
+
+/**
+ * 取出最终哈希摘要计算结果数据缓冲区指针
+ */
+const BYTE *HashSequenceCompleteCommand::getResponseDigestValue() const {
+    return result.t.buffer;
+}
+
+/**
+ * 取出最终哈希摘要计算结果数据缓冲区的长度, 单位字节
+ */
+UINT16 HashSequenceCompleteCommand::getResponseDigestSize() const {
+    return result.t.size;
+}
+
+/**
+ * 取出最终哈希摘要计算结果 TPM2B 结构体
+ */
+const TPM2B_DIGEST& HashSequenceCompleteCommand::getResponseDigest() const {
+    return result;
+}
+
+/**
+ * 指定哈希操作输出凭据中引用的 hierarchy 树
+ *
+ * (参数及返回值定义请以头文件中的定义为准)
+ */
+TPMI_RH_HIERARCHY HashSequenceCompleteCommand::setHierarchyForValidationTicket(TPMI_RH_HIERARCHY hierarchy) {
+    this->hierarchy = hierarchy;
+    return hierarchy;
+}
+ /**
+ * 取出证明哈希摘要是由 TPM 模块输出的凭据
+ *
+ * @return TPMT_TK_HASHCHECK (C++引用) 凭据
+ */
+const TPMT_TK_HASHCHECK& HashSequenceCompleteCommand::getResponseValidationTicket() const {
+    return validationTicket;
+}
+
--- HashSequenceScheduler.h
+++ HashSequenceScheduler.h
@@ -126,4 +126,102 @@ public:
 
 };
 
+/**
+ * C++ 类名: HashSequenceCompleteCommand
+ *
+ * 设计意图:
+ * 将 Tss2_Sys_SequenceComplete() 与自己的所有调用参数捆绑到一起,
+ * 从而避免客户程序调用该函数时必须创建一堆零散的数据结构引入过高的复杂度的问题.
+ */
+class HashSequenceCompleteCommand {
+private:
+    TPMI_DH_OBJECT sequenceHandle;
+    TPMS_AUTH_COMMAND sessionData;
+    TPM2B_MAX_BUFFER data; // 用于存储最后一轮哈希操作中待处理的原始数据 (可以为空)
+    TPMI_RH_HIERARCHY hierarchy; // The hierarchy of the ticket for a hash operation
+    // 以上成员变量用于存储输入数据
+    // 以下用于记录输出结果
+    TPM2B_DIGEST result; // 哈希摘要运算结果
+    TPMT_TK_HASHCHECK validationTicket; // 附带一份哈希结果有效性证明, 用于后续调用相关命令对摘要结果进行签名
+    TPM_RC rc;
+
+public:
+    /**
+     * 构造函数
+     */
+    HashSequenceCompleteCommand();
+
+    /**
+     * 析构函数
+     */
+    virtual ~HashSequenceCompleteCommand();
+
+    /**
+     * 指定哈希序列句柄及句柄本身的访问授权 AuthValue
+     */
+    void setSequenceHandle(TPMI_DH_OBJECT sequenceHandle);
+    void setSequenceHandleWithOptionalAuthValue(
+            TPMI_DH_OBJECT sequenceHandle, // 句柄
+            BYTE authValue[], // 句柄授权数据
+            UINT16 size // 数据长度
+            );
+    void clearAuthValue();
+
+    /**
+     * 存入待进行哈希计算的最后一个数据包
+     *
+     * @param data 待哈希的数据包
+     * @param size 如果数据长度超过 MAX_DIGEST_BUFFER=1024 字节, 这里将自动截断多余的字节
+     * @return TPM2B 格式的数据块, 仅为调试使用, 可直接忽略该返回值
+     */
+    const TPM2B_MAX_BUFFER& prepareFinalDataPack(const BYTE data[], UINT16 size);
+
+    /**
+     * 清除数据(清除运行时缓存的最后一个待计算的原始数据包)
+     */
+    void clearFinalDataPack();
+
+    /**
+     * 执行 TPM 命令
+     */
+    virtual void execute(TSS2_SYS_CONTEXT *pSysContext);
+
+    /**
+     * 取出最终哈希摘要计算结果数据缓冲区指针
+     *
+     * @return 缓冲区首地址指针
+     */
+    const BYTE *getResponseDigestValue() const;
+
+    /**
+     * 取出最终哈希摘要计算结果数据缓冲区的长度, 单位字节
+     *
+     * @return 长度
+     */
+    UINT16 getResponseDigestSize() const;
+
+    /**
+     * 取出最终哈希摘要计算结果 TPM2B 结构体
+     *
+     * @return TPM2B_DIGEST (C++引用)
+     */
+    const TPM2B_DIGEST& getResponseDigest() const;
+
+    /**
+     * 指定哈希操作输出凭据中引用的 hierarchy 树
+     *
+     * @param hierarchy 可以选择 TPM_RH_PLATFORM 或 TPM_RH_OWNER 等.
+     *        调用此函数前, hierarchy 的默认值由构造函数设置, 通常取 TPM_RH_PLATFORM
+     * @return TPMI_RH_HIERARCHY 仅用于方便调试
+     */
+    TPMI_RH_HIERARCHY setHierarchyForValidationTicket(TPMI_RH_HIERARCHY hierarchy);
+
+    /**
+     * 取出证明哈希摘要是由 TPM 模块输出的凭据
+     *
+     * @return TPMT_TK_HASHCHECK (C++引用) 凭据
+     */
+    const TPMT_TK_HASHCHECK& getResponseValidationTicket() const;
+};
+
 #endif /* HASH_SEQUENCE_SCHEDULER_H_ */
